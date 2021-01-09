50256da164fe9ca0985e8646ebea1c52317e7f6f
GuillermoDuran <gadm96.m01@gmail.com>
2021-01-09 14:40:35 -0500

    Consulta y registro de ninos desde usuario normal

--- js/userLogic.js
+++ js/userLogic.js
@@ -43,6 +43,7 @@ window.onload = function () {
             }
         }
 
+    //Guardar el formulario
     form.addEventListener("submit", function (e) {
 
         e.preventDefault();
@@ -61,6 +62,7 @@ window.onload = function () {
 
     var contaioner_cons = document.getElementsByName("container-input-cons");
     
+    //Boton de CONSULTA
     consult_btn.addEventListener("click", function () {
         document.getElementById("frm_user_wrapper").style.display = "none";
         if(ninos == null) {
@@ -69,66 +71,11 @@ window.onload = function () {
             edad_nino[0].innerText = "No hay datos";
             tiempo_nino[0].innerText = "No hay datos";
             curso_nino[0].innerText = "No hay datos";
-        }/*else{
-            ninosRef.once('value')
-            .then(function (snapshot) {
-                var childKeys = Object.keys(snapshot.val());
-                for (i = 0; i < childKeys.length; i++) {
-                    var ninoNombre = snapshot.child(childKeys[i] + "/nombre").val();
-                    var ninoApell = snapshot.child(childKeys[i] + "/apellido").val();
-                    var ninoEdad = snapshot.child(childKeys[i] + "/edad").val();
-                    var ninoTiempo = snapshot.child(childKeys[i] + "/tiempo").val();
-                    var ninoCurso = snapshot.child(childKeys[i] + "/curso").val();
-                    if (childKeys.length === 0) {
-                        userId = childKeys[i]
-                        nombre_nino[i].innerText = ninoNombre;
-                        apell_nino[i].innerText = ninoApell;
-                        edad_nino[i].innerText = ninoEdad;
-                        tiempo_nino[i].innerText = ninoTiempo;
-                        curso_nino[i].innerText = ninoCurso;
-                        break;
-                    }else{
-                        contaioner_cons[i].insertAdjacentHTML("afterend", 
-                        `<div class="container-inptbxs" name="container-input-cons">
-                            <p>
-                                <label class="req" for="name1">Nombre del niño</label>
-                                <span class="name_nino" name="name1-cons" id="name-nino"> Nombre nino </span>
-                            </p>
-                
-                            <p>
-                                <label class="req" for="apell1">Apellido del niño</label>
-                                <span class="apell_ninos" name="apell1-cons" id="apell-nino"> Apellido </span>
-                            </p>
-                
-                            <p>
-                                <label class="req" for="edad1">Edad del niño</label>
-                                <span class="edad_ninos" name="edad1-cons" id="edad-nino"> Edad nino </span>
-                            </p>
-                
-                            <p>
-                                <label class="req" for="time_av1">Tiempo disponible del niño</label>
-                                <span class="time_ninos" name="time_av1-cons" id="time_av_nino"> Tiempo </span>
-                            </p>
-                
-                            <p>
-                                <label class="req" for="curso-nino">Curso</label>
-                                <span class="curso-ninos" name="curso-nino-cons" id="curso-nino"> Curso nino </span>
-                            </p>
-                        </div>`);
-                        nombre_nino[i].innerText = ninoNombre;
-                        apell_nino[i].innerText = ninoApell;
-                        edad_nino[i].innerText = ninoEdad;
-                        tiempo_nino[i].innerText = ninoTiempo;
-                        curso_nino[i].innerText = ninoCurso;
-                    }
-                }
-            }).catch((error) => {
-                console.log(error);
-            });
-        }*/
+        }
         document.getElementById("consulta").style.display = "inherit";
     });
 
+    //GURDAR datos de los ninos
     function escribirDatosNinos () {
 
         data = {
@@ -141,63 +88,69 @@ window.onload = function () {
 
         if(ninos == null) {
             console.log(ninos)
-            ref.child("ninos").push().set(data);
+            ref.child("ninos").push().set(data)
+                .then(ninosRef.once('value')
+                .then(function (snapshot) {
+                    var childKeys = Object.keys(snapshot.val());
+                    console.log(childKeys.length)
+                    var ninoNombre = snapshot.child(childKeys[0] + "/nombre").val();
+                    var ninoApell = snapshot.child(childKeys[0] + "/apellido").val();
+                    var ninoEdad = snapshot.child(childKeys[0] + "/edad").val();
+                    var ninoTiempo = snapshot.child(childKeys[0] + "/tiempoDisp").val();
+                    var ninoCurso = snapshot.child(childKeys[0] + "/curso").val();
+                    nombre_nino[0].innerText = ninoNombre;
+                    apell_nino[0].innerText = ninoApell;
+                    edad_nino[0].innerText = ninoEdad;
+                    tiempo_nino[0].innerText = ninoTiempo;
+                    curso_nino[0].innerText = ninoCurso;
+                }).catch((e) => {
+                    console.log(e);
+                })
+                .then(ref.once("value")
+                .then(function (snapshot) {
+                    ninos = snapshot.child("ninos").val();
+                })));
         }else{ 
-            ninosRef.once('value')
+            console.log(ninos)
+            ref.child("ninos").push().set(data)
+            .then(ninosRef.once('value')
             .then(function (snapshot) {
                 var childKeys = Object.keys(snapshot.val());
-                for (i = 0; i < childKeys.length; i++) {
-                    var ninoNombre = snapshot.child(childKeys[i] + "/nombre").val();
-                    var ninoApell = snapshot.child(childKeys[i] + "/apellido").val();
-                    var ninoEdad = snapshot.child(childKeys[i] + "/edad").val();
-                    var ninoTiempo = snapshot.child(childKeys[i] + "/tiempo").val();
-                    var ninoCurso = snapshot.child(childKeys[i] + "/curso").val();
-                    if (childKeys.length === 0) {
-                        userId = childKeys[i]
-                        nombre_nino[i].innerText = ninoNombre;
-                        apell_nino[i].innerText = ninoApell;
-                        edad_nino[i].innerText = ninoEdad;
-                        tiempo_nino[i].innerText = ninoTiempo;
-                        curso_nino[i].innerText = ninoCurso;
-                        break;
-                    }else{
-                        contaioner_cons[i].insertAdjacentHTML("afterend", 
-                        `<div class="container-inptbxs" name="container-input-cons">
-                            <p>
-                                <label class="req" for="name1">Nombre del niño</label>
-                                <span class="name_nino" name="name1-cons" id="name-nino"> Nombre nino </span>
-                            </p>
-                
-                            <p>
-                                <label class="req" for="apell1">Apellido del niño</label>
-                                <span class="apell_ninos" name="apell1-cons" id="apell-nino"> Apellido </span>
-                            </p>
-                
-                            <p>
-                                <label class="req" for="edad1">Edad del niño</label>
-                                <span class="edad_ninos" name="edad1-cons" id="edad-nino"> Edad nino </span>
-                            </p>
-                
-                            <p>
-                                <label class="req" for="time_av1">Tiempo disponible del niño</label>
-                                <span class="time_ninos" name="time_av1-cons" id="time_av_nino"> Tiempo </span>
-                            </p>
-                
-                            <p>
-                                <label class="req" for="curso-nino">Curso</label>
-                                <span class="curso-ninos" name="curso-nino-cons" id="curso-nino"> Curso nino </span>
-                            </p>
-                        </div>`);
-                        nombre_nino[i].innerText = ninoNombre;
-                        apell_nino[i].innerText = ninoApell;
-                        edad_nino[i].innerText = ninoEdad;
-                        tiempo_nino[i].innerText = ninoTiempo;
-                        curso_nino[i].innerText = ninoCurso;
-                    }
-                }
-            }).catch((error) => {
-                console.log(error);
-            });
+                contaioner_cons[contaioner_cons.length-1].insertAdjacentHTML("afterend", 
+                `<div class="container-inptbxs" name="container-input-cons">
+                    <p>
+                        <label class="req" for="name1">Nombre del niño</label>
+                        <span class="name_nino" name="name1-cons" id="name-nino"> Nombre nino </span>
+                    </p>
+        
+                    <p>
+                        <label class="req" for="apell1">Apellido del niño</label>
+                        <span class="apell_ninos" name="apell1-cons" id="apell-nino"> Apellido </span>
+                    </p>
+        
+                    <p>
+                        <label class="req" for="edad1">Edad del niño</label>
+                        <span class="edad_ninos" name="edad1-cons" id="edad-nino"> Edad nino </span>
+                    </p>
+        
+                    <p>
+                        <label class="req" for="time_av1">Tiempo disponible del niño</label>
+                        <span class="time_ninos" name="time_av1-cons" id="time_av_nino"> Tiempo </span>
+                    </p>
+        
+                    <p>
+                        <label class="req" for="curso-nino">Curso</label>
+                        <span class="curso-ninos" name="curso-nino-cons" id="curso-nino"> Curso nino </span>
+                    </p>
+                </div>`);
+                snapshot.forEach(function(childSnap) {
+                    nombre_nino[childKeys.length-1].innerText = childSnap.val().nombre;
+                    apell_nino[childKeys.length-1].innerText = childSnap.val().apellido;
+                    edad_nino[childKeys.length-1].innerText = childSnap.val().edad;
+                    tiempo_nino[childKeys.length-1].innerText = childSnap.val().tiempoDisp;
+                    curso_nino[childKeys.length-1].innerText = childSnap.val().curso;
+                })
+            }));
         }
     }
 
@@ -207,26 +160,24 @@ window.onload = function () {
         document.getElementById("frm_user_wrapper").style.display = "inherit";
     });
 
+    //MUESTRA LOS DATOS DE LA CONSULTA
     ninosRef.once('value')
         .then(function (snapshot) {
-            console.log(snapshot.val())
             var childKeys = Object.keys(snapshot.val());
             for (i = 0; i < childKeys.length; i++) {
                 var ninoNombre = snapshot.child(childKeys[i] + "/nombre").val();
                 var ninoApell = snapshot.child(childKeys[i] + "/apellido").val();
                 var ninoEdad = snapshot.child(childKeys[i] + "/edad").val();
-                var ninoTiempo = snapshot.child(childKeys[i] + "/tiempo").val();
+                var ninoTiempo = snapshot.child(childKeys[i] + "/tiempoDisp").val();
                 var ninoCurso = snapshot.child(childKeys[i] + "/curso").val();
-                if (childKeys.indexOf(childKeys[i]) == 0) {
-                    userId = childKeys[i]
+                if (i === 0) {
                     nombre_nino[i].innerText = ninoNombre;
                     apell_nino[i].innerText = ninoApell;
                     edad_nino[i].innerText = ninoEdad;
                     tiempo_nino[i].innerText = ninoTiempo;
                     curso_nino[i].innerText = ninoCurso;
-                    break;
                 }else{
-                    contaioner_cons[i].insertAdjacentHTML("afterend", 
+                    contaioner_cons[contaioner_cons.length-1].insertAdjacentHTML("afterend", 
                     `<div class="container-inptbxs" name="container-input-cons">
                         <p>
                             <label class="req" for="name1">Nombre del niño</label>
@@ -253,11 +204,11 @@ window.onload = function () {
                             <span class="curso-ninos" name="curso-nino-cons" id="curso-nino"> Curso nino </span>
                         </p>
                     </div>`);
-                    nombre_nino[i].innerText = ninoNombre;
-                    apell_nino[i].innerText = ninoApell;
-                    edad_nino[i].innerText = ninoEdad;
-                    tiempo_nino[i].innerText = ninoTiempo;
-                    curso_nino[i].innerText = ninoCurso;
+                    nombre_nino[contaioner_cons.length-1].innerText = ninoNombre;
+                    apell_nino[contaioner_cons.length-1].innerText = ninoApell;
+                    edad_nino[contaioner_cons.length-1].innerText = ninoEdad;
+                    tiempo_nino[contaioner_cons.length-1].innerText = ninoTiempo;
+                    curso_nino[contaioner_cons.length-1].innerText = ninoCurso;
                 }
             }
         }).catch((error) => {
